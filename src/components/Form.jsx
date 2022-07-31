97121e539e97aeb0cc2b58b3e01b2f8e1be946ad
Juan Galarce <j6alarce@gmail.com>
2022-07-30 20:58:36 -0400

    Se crean los campos y input del formulario con sus estilos

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <div className="contenedor">
+      <form>
+        <div className="campo">
+          <label htmlFor="city">Ciudad</label>
+          <input type="text" id="city" />
+        </div>
+        <div className="campo">
+          <label htmlFor="city">Pais</label>
+          <select id="country" name="country">
+            <option value="">-- Seleccione tu país --</option>
+            <option value="CL">Chile</option>
+            <option value="US">Estados Unidos</option>
+            <option value="AR">Argentina</option>
+            <option value="MX">Mexico</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Peru</option>
+          </select>
+        </div>
+        <input type="submit" value="Consultar Clima" />
+      </form>
+    </div>
+  );
+};
 
-export default Form
+export default Form;
